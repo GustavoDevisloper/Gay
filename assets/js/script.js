64e93d60d87fed6e884a37286d01ec6231a01a6e
GustavoDevisloper <morenubrabo@gmail.com>
2023-07-19 03:42:35 -0300

    #4

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ function showMessage() {
         window.location.href = "https://www.tiktok.com/@sillas_xs/video/7240592303998422278?is_from_webapp=1&sender_device=pc";
     }
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+    var buttonNo = document.querySelector(".buttonNo");
+    var originalTransform = buttonNo.style.transform;
+    var isTransformed = false;
+  
+    buttonNo.addEventListener("click", function() {
+      if (isTransformed) {
+        buttonNo.style.transform = originalTransform;
+      } else {
+        buttonNo.style.transform = "translateY(200px)";
+      }
+      isTransformed = !isTransformed;
+    });
+  });
+  

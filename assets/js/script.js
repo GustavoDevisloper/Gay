137b0d6392b1434b66560dcb2f367540f46b1ceb
GustavoDevisloper <morenubrabo@gmail.com>
2023-07-19 18:32:32 -0300

    update new

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function showMessage() {
     var confirmation = confirm("Eu já sabia, mas obrigado por confirmar ❤️");
 
     if (confirmation) {
-        window.location.href = "https://imgur.com/a/3T4Xc1Z";
+        window.location.href = "https://www.tiktok.com/@sillas_xs/video/7240592303998422278?is_from_webapp=1&sender_device=pc";
     }
 }
 

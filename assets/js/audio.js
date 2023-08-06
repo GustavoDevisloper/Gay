b934442d18f272a972b23b1d5986dd4cae13b2db
GustavoDevisloper <morenubrabo@gmail.com>
2023-08-06 17:56:57 -0300

    fica cmg edited

--- assets/js/audio.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    playAudio();
-  });
-  
-  function playAudio() {
-    var audio = new Audio("assets/music.mp3");
-    audio.play();
-  }
-  
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function showMessage() {
     var confirmation = confirm("Eu já sabia, mas obrigado por confirmar ❤️");
 
     if (confirmation) {
-        window.location.href = "gay.html";
+        window.location.href = "https://www.tiktok.com/@th1agoribeir0/video/7232812756762643717?q=vai%20ficar%20comigo%20nao%20%3F%20&t=1691355363311";
     }
 }
 
